713f9291b42b73bc8659b43ba514884bc4df1fa0
Mateus Sousa <mateuseng_ec@alu.ufc.br>
2018-10-25 16:36:40 -0300

    Update LowButtFilter.m

--- LowButtFilter/LowButtFilter.m
+++ LowButtFilter/LowButtFilter.m
@@ -1,12 +1,43 @@
+% Projeto de Filtro Passa Baixas de Butterworth
+
+% Especificações:
+
+% Frequencia de passagem wp = 300 Hz 
+% Frequencia de rejeição ws = 750 Hz
+% Perda de amplitude na faixa de passagem = -0.2228 dB
+% Perda de amplitude na faixa de rejeição = -13.01 dB
+
+% Fases do projeto:
+% 0. Especificações
+% 1. Encontrar a ordem (N)
+% 2. Descobrir wc
+% 3. Montar a função H(s)
+% 4. Implementação
+
+% Ordem do filtro:
+% N = log(((2*epsilon - epsilon^2)*delta^2)/((1-epsilon)^2*(1-delta^2)))/(2*log(wp/ws))
+% Arredondar N para o inteiro mais próximo
+
+% Frequência de corte:
+% Faixa de Passagem: wc = wp/((2*epsilon - epsilon^2)/(1-epsilon)^2)^(1/(2*N))
+% Faixa de Rejeição: wc = ws/((1-delta^2)/delta^2)^(1/(2*N)) 
+% Fazer média entre os dois
+
+% Montar H(s)
+% H(s) = (wc^N)/Qn(s)
+% O polinômio Qn(s) depende da ordem e já se encontra tabelado
+
 %|H(jw)| -> Achar função de transferência do filtro
 
-% obtendo as especificaçõespecifica
+% obtendo as especificações
+
+% Hz -> ciclos/seg -> 2*pi/s
 
 wp = 300*2*pi;                  % frequencia de passagem [rad/s]
 ws = 750*2*pi;                  % frequencia de rejeição [rad/s]
 
-epsilon = 1-10^(-0.2228/20);    % 20log(1-eps) = -0,2228 dB
-delta = 10^(-13.01/20);         % 20log(del) = -13,01 dB
+epsilon = 1-10^(-0.2228/20);    % 20log(1-eps) = -0.2228 dB
+delta = 10^(-13.01/20);         % 20log(del) = -13.01 dB
 
 % obtenção da ordem do filtro:
 N = log10 (((2*epsilon - epsilon^2)*delta^2)/((1-epsilon)^2*(1-delta^2)))/(2*log10(wp/ws));
@@ -21,15 +52,15 @@ wc = (wcp + wcs)/2;             % media entre os dois
 % função de transferência do filtro de Butterworth ordem 4
 
 num = wc^4;                     % criação de um numerador
-den = [1 2.6131*wc 3.4142*wc^2 2.6131*wc^3 wc^4];
-Hs = tf(num, den);              % analógico -> digital
+den = [1 2.6131*wc 3.4142*wc^2 2.6131*wc^3 wc^4]; % criação de um denominador
+Hs = tf(num, den);              % transformação (analógico -> digital)
 
 % discretização de Tustin (IIR)
 
-filename = 'Black Sabbath - Iron Man.wav';
+filename = 'big-ben-strikes-12-good-quality-sound.wav';
 [som, freq] = audioread (filename);  % obtem arquivo e freq. do audio
 
-Ts = 1/freq;
+Ts = 1/freq;                    % definição do período 
 
 IIR = c2d(Hs, Ts, 'tustin');    % aproximação de Tustin
 [IIRnum, IIRden] = tfdata(IIR, 'v');
@@ -38,6 +69,4 @@ IIR = c2d(Hs, Ts, 'tustin');    % aproximação de Tustin
 
 somlowpass = filter(IIRnum, IIRden, som); % aplica o filtro
 
-audiowrite('iron.wav', somlowpass, freq);
-
-
+audiowrite('result.wav', somlowpass, freq); % gera o audio filtrado
