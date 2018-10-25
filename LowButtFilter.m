06c972329195314fc60b8c23b19972ab89aa6408
Mateus Sousa <mateuseng_ec@alu.ufc.br>
2018-10-25 11:43:06 -0300

    Update LowButtFilter.m

--- LowButtFilter.m
+++ LowButtFilter.m
@@ -1,6 +1,16 @@
+% Projeto de Filtro Passa Baixas de Butterworth
+
+% Especificações:
+
+% Frequencia de passagem wp = 300 Hz 
+% Frequencia de rejeição ws = 750 Hz
+% Perda de amplitude na faixa de passagem = -0,2228 dB
+% Perda de amplitude na faixa de rejeição = -13.01 dB
+
+
 %|H(jw)| -> Achar função de transferência do filtro
 
-% obtendo as especificaçõespecifica
+% obtendo as especificações
 
 wp = 300*2*pi;                  % frequencia de passagem [rad/s]
 ws = 750*2*pi;                  % frequencia de rejeição [rad/s]
@@ -21,15 +31,15 @@ wc = (wcp + wcs)/2;             % media entre os dois
 % função de transferência do filtro de Butterworth ordem 4
 
 num = wc^4;                     % criação de um numerador
-den = [1 2.6131*wc 3.4142*wc^2 2.6131*wc^3 wc^4];
-Hs = tf(num, den);              % analógico -> digital
+den = [1 2.6131*wc 3.4142*wc^2 2.6131*wc^3 wc^4]; % criação de um denominador
+Hs = tf(num, den);              % transformação (analógico -> digital)
 
 % discretização de Tustin (IIR)
 
 filename = 'big-ben-strikes-12-good-quality-sound.wav';
 [som, freq] = audioread (filename);  % obtem arquivo e freq. do audio
 
-Ts = 1/freq;
+Ts = 1/freq;                    % definição do período 
 
 IIR = c2d(Hs, Ts, 'tustin');    % aproximação de Tustin
 [IIRnum, IIRden] = tfdata(IIR, 'v');
@@ -38,6 +48,6 @@ IIR = c2d(Hs, Ts, 'tustin');    % aproximação de Tustin
 
 somlowpass = filter(IIRnum, IIRden, som); % aplica o filtro
 
-audiowrite('result.wav', somlowpass, freq);
+audiowrite('result.wav', somlowpass, freq); % gera o audio filtrado
 
 
